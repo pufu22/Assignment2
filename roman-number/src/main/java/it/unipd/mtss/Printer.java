a2a5bd2a99ad3dd8635de265cb6616a4a203fd31
ZhengZW1999 <z15389069437@gmail.com>
2023-05-13 17:34:56 +0200

    printer

---
+++ roman-number/src/main/java/it/unipd/mtss/Printer.java
@@ -0,0 +1,78 @@
+package it.unipd.mtss;
+
+public class Printer {
+    /**
+     * @param romanNumber
+     * @return
+     */
+    private static String printAsciiArt(String romanNumber) {
+        StringBuilder result = new StringBuilder();
+        // Cicla su ogni carattere del numero romano
+        for (int i = 0; i < romanNumber.length(); i++) {
+            char c = romanNumber.charAt(i);
+            // Aggiungi l'ASCII art corrispondente al carattere
+            switch (c) {
+                case 'I':
+                    result.append("  _____ \n");
+                    result.append(" |_   _|\n");
+                    result.append("   | |  \n");
+                    result.append("   | |  \n");
+                    result.append("  _| |_ \n");
+                    result.append(" |_____|\n");
+                    break;
+                case 'V':
+                    result.append(" __      __\n");
+                    result.append(" \\ \\    / /\n");
+                    result.append("  \\ \\  / / \n");
+                    result.append("   \\ \\/ /  \n");
+                    result.append("    \\  /   \n");
+                    result.append("     \\/    \n");
+                    break;
+                case 'X':
+                    result.append(" __   __\n");
+                    result.append(" \\ \\ / /\n");
+                    result.append("  \\ V / \n");
+                    result.append("   > <  \n");
+                    result.append("  / . \\ \n");
+                    result.append(" /_/ \\_\\\n");
+                    break;
+                case 'L':
+                    result.append("  _      \n");
+                    result.append(" | |     \n");
+                    result.append(" | |     \n");
+                    result.append(" | |     \n");
+                    result.append(" | |____ \n");
+                    result.append(" |______|\n");
+                    break;
+                case 'C':
+                    result.append("   _____ \n");
+                    result.append("  / ____|\n");
+                    result.append(" | |     \n");
+                    result.append(" | |     \n");
+                    result.append(" | |____ \n");
+                    result.append("  \\_____|\n");
+                    break;
+                case 'D':
+                    result.append("  _____  \n");
+                    result.append(" |  __ \\ \n");
+                    result.append(" | |  | |\n");
+                    result.append(" | |  | |\n");
+                    result.append(" | |__| |\n");
+                    result.append(" |_____/ \n");
+                    break;
+                case 'M':
+                    result.append("  __  __ \n");
+                    result.append(" |  \\/  |\n");
+                    result.append(" | |\\/| |\n");
+                    result.append(" | |  | |\n");
+                    result.append(" | |  | |\n");
+                    result.append(" |_|  |_|\n");
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid Roman numeral: " + c);
+            }
+        }
+        return result.toString();
+    }
+    
+}
