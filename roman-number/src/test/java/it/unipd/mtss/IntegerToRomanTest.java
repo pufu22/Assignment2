5cd706d39ae09f7a1b558b3e20d43ee2e601b271
jude <pufu22@gmail.com>
2023-05-15 22:27:24 +0200

    Implementati test IntegerToRoman

--- roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.junit.Test;
+import org.junit.validator.PublicClassValidator;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
@@ -25,6 +26,46 @@ public class IntegerToRomanTest {
         assertEquals(IntegerToRoman.convert(1),"I");
     }
     
+    @Test 
+    public void testConvertThousand(){
+        assertEquals(IntegerToRoman.convert(1000),"M");
 
+        try(MockedStatic<IntegerToRoman> converter = 
+        Mockito.mockStatic(IntegerToRoman.class)) {
+            converter.when(() -> IntegerToRoman.convert(1000))
+            .thenReturn("M");
+            assertEquals(IntegerToRoman.convert(1000),"M");
+        }
 
+        assertEquals(IntegerToRoman.convert(1000),"M");
+    }
+
+    @Test
+    public void testConvertCentinaia(){
+        assertEquals(IntegerToRoman.convert(100),"C");
+
+        try(MockedStatic<IntegerToRoman> converter = 
+        Mockito.mockStatic(IntegerToRoman.class)) {
+            converter.when(() -> IntegerToRoman.convert(100))
+            .thenReturn("C");
+            assertEquals(IntegerToRoman.convert(100),"C");
+        }
+
+        assertEquals(IntegerToRoman.convert(100),"C");
+    }
+
+    @Test
+    public void testConvertDecine(){
+        assertEquals(IntegerToRoman.convert(10),"X");
+
+        try(MockedStatic<IntegerToRoman> converter = 
+        Mockito.mockStatic(IntegerToRoman.class)) {
+            converter.when(() -> IntegerToRoman.convert(10))
+            .thenReturn("X");
+            assertEquals(IntegerToRoman.convert(10),"X");
+        }
+
+        assertEquals(IntegerToRoman.convert(10),"X");
+    }
+    
 }
